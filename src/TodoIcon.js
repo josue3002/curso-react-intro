a6dc4d06c049943fbb96793090c39b46bb7e9fc5
josue3002 <josuegullo163@gmail.com>
2024-05-30 19:35:09 -0400

    Agregando SVGs

---
+++ src/TodoIcon.js
@@ -0,0 +1,19 @@
+import { ReactComponent as CheckSVG } from './check.svg';
+import { ReactComponent as DeleteSVG } from './delete.svg';
+
+const iconType = {
+    "check": <CheckSVG />,
+    "delete": <DeleteSVG />,
+}
+
+function TodoIcon({ type }) {
+    return (
+        <span 
+        className={`Icon Icon-svg Icon-${type}`}
+        >
+            {iconType[type]}
+        </span>
+    )
+}
+
+export { TodoIcon };
